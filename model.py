2bd4f522342b1c9de71d27b8c869967b31e52365
SiyuanL <58480248+LINSY97@users.noreply.github.com>
2021-03-09 17:50:49 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -3,16 +3,40 @@ import pretrainedmodels
 import pretrainedmodels.utils
 
 
-def get_model(model_name="se_resnext50_32x4d", num_classes=101, pretrained="imagenet"):
-    model = pretrainedmodels.__dict__[model_name](pretrained=pretrained)
-    dim_feats = model.last_linear.in_features
-    model.last_linear = nn.Linear(dim_feats, num_classes)
-    model.avg_pool = nn.AdaptiveAvgPool2d(1)
-    return model
+class MODEL(nn.Module):
+    def __init__(self, model_name="se_resnext50_32x4d", num_classes_age=101, 
+                  num_classes_gender=2, pretrained="imagenet"):
+        super(MODEL, self).__init__()
+        img_model = pretrainedmodels.__dict__[model_name](pretrained=pretrained)
+        img_model.avg_pool = nn.AdaptiveAvgPool2d(1)
+        encoder_size = img_model.last_linear.in_features
+        list_layer = list(img_model.children())[:-1]
+        self.encoder = nn.Sequential(*list_layer)
+
+        self.head1 = nn.Sequential(
+          nn.Linear(encoder_size, 500),
+          nn.ReLU(),
+          nn.Linear(500, num_classes_age)
+        )
+
+        self.head2 = nn.Sequential(
+          nn.Linear(encoder_size, 500),
+          nn.ReLU(),
+          nn.Linear(500, num_classes_gender)
+        )
+
+    def forward(self, x):
+        shared_features = self.encoder(x)
+        shared_features = shared_features.squeeze()
+        out1 = self.head1(shared_features)
+        # print("yes out_1")
+        out2 = self.head2(shared_features)
+        # print("yes out_2")
+        return out1, out2
 
 
 def main():
-    model = get_model()
+    model = MODEL()
     print(model)
 
 
